31a3f587c82a3d5e00320a8039597f2505e0c7db
Elena Mikhailova <elmikhailova86@gmail.com>
2026-08-27 21:29:20 +0300

    test: add parameterized login validation

--- src/test/java/com/github/elenamikhailova/automation/api/data/UserData.java
+++ src/test/java/com/github/elenamikhailova/automation/api/data/UserData.java
@@ -2,6 +2,9 @@ package com.github.elenamikhailova.automation.api.data;
 
 import com.github.elenamikhailova.automation.api.model.CreateUserRequest;
 import net.datafaker.Faker;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
 
 public class UserData {
     private final Faker faker = new Faker();
@@ -26,5 +29,67 @@ public class UserData {
                 .city(faker.address().city())
                 .mobileNumber(faker.phoneNumber().cellPhone())
                 .build();
+    }
+
+    public static Stream<Arguments> invalidLoginCases() {
+        UserData userData = new UserData();
+        CreateUserRequest user1 = userData.generateRandomUser();
+        CreateUserRequest user2 = userData.generateRandomUser();
+        CreateUserRequest user3 = userData.generateRandomUser();
+        CreateUserRequest user4 = userData.generateRandomUser();
+        CreateUserRequest user5 = userData.generateRandomUser();
+        CreateUserRequest user6 = userData.generateRandomUser();
+
+        return Stream.of(
+                Arguments.of(
+                        "Empty email",
+                        user1,
+                        "",
+                        user1.getPassword(),
+                        404,
+                        "User not found!"
+                ),
+                Arguments.of(
+                        "Empty password",
+                        user2,
+                        user2.getEmail(),
+                        "",
+                        404,
+                        "User not found!"
+                ),
+                Arguments.of(
+                        "Invalid email",
+                        user3,
+                        "invalid_email",
+                        user3.getPassword(),
+                        404,
+                        "User not found!"
+                ),
+                Arguments.of(
+                        "Invalid password",
+                        user4,
+                        user4.getEmail(),
+                        user4.getPassword() + "_inv",
+                        404,
+                        "User not found!"
+                ),
+                Arguments.of(
+                        "Without email",
+                        user5,
+                        null,
+                        user5.getPassword(),
+                        400,
+                        "Bad request, email or password parameter is missing in POST request."
+                ),
+                Arguments.of(
+                        "Without password",
+                        user6,
+                        user6.getEmail(),
+                        null,
+                        400,
+                        "Bad request, email or password parameter is missing in POST request."
+                )
+
+        );
     }
 }
