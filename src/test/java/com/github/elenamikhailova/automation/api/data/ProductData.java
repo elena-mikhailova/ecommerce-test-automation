08350d158568ba6f807827302d368803a402dcbf
Elena Mikhailova <elmikhailova86@gmail.com>
2026-08-28 18:14:54 +0300

    refactor: remove hardcoded API test data

--- src/test/java/com/github/elenamikhailova/automation/api/data/ProductData.java
+++ src/test/java/com/github/elenamikhailova/automation/api/data/ProductData.java
@@ -1,4 +1,11 @@
 package com.github.elenamikhailova.automation.api.data;
 
+import net.datafaker.Faker;
+
 public class ProductData {
+    private final Faker faker = new Faker();
+
+    public String generateSearchTerm() {
+        return faker.options().option("top", "tshirt", "jean");
+    }
 }
--- src/test/java/com/github/elenamikhailova/automation/api/tests/ProductsApiTest.java
+++ src/test/java/com/github/elenamikhailova/automation/api/tests/ProductsApiTest.java
@@ -1,6 +1,7 @@
 package com.github.elenamikhailova.automation.api.tests;
 
 import com.github.elenamikhailova.automation.api.client.ProductsApiClient;
+import com.github.elenamikhailova.automation.api.data.ProductData;
 import com.github.elenamikhailova.automation.base.BaseApiTest;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +13,7 @@ import static org.hamcrest.Matchers.*;
 
 public class ProductsApiTest extends BaseApiTest {
     private ProductsApiClient productsApiClient;
+    private ProductData productData = new ProductData();
 
     @BeforeEach
     void setUp() {
@@ -32,12 +34,13 @@ public class ProductsApiTest extends BaseApiTest {
     @Test
     @DisplayName("POST /searchProduct returns a product")
     void canSearchProducts() {
-        Response response = productsApiClient.searchProduct("top");
+        String searchTerm = productData.generateSearchTerm();
+        Response response = productsApiClient.searchProduct(searchTerm);
         response.then()
                 .statusCode(200)
                 .body("responseCode", equalTo(200))
                 .body("products", not(empty()))
-                .body("products.name", hasItem(containsStringIgnoringCase("top")));
+                .body("products.name", hasItem(containsStringIgnoringCase(searchTerm)));
 
     }
 }
