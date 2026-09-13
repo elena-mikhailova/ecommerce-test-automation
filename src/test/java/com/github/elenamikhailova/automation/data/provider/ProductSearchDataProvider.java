9ca43f34f90ed6b128b6b4d677cf15ec81ee9e93
Elena Mikhailova <elmikhailova86@gmail.com>
2026-09-13 19:23:49 +0300

    test: improve product search parameterization

---
+++ src/test/java/com/github/elenamikhailova/automation/data/provider/ProductSearchDataProvider.java
@@ -0,0 +1,27 @@
+package com.github.elenamikhailova.automation.data.provider;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
+public class ProductSearchDataProvider {
+
+    public static Stream<Arguments> validSearchCases() {
+        return Stream.of(
+                Arguments.of(
+                        "Search for top", "top"),
+                Arguments.of(
+                        "Search for tshirt", "tshirt"),
+                Arguments.of(
+                        "Search for Search for jean", "jean")
+        );
+    }
+
+    public static Stream<Arguments> inValidSearchCases() {
+        return Stream.of(
+                Arguments.of(
+                        "Search for nonexistent product", "hgytoe")
+        );
+    }
+
+}
